a01be7e24b95500049c4680342507828f2bcda43
Arthur Santos <arthursantosgc@gmail.com>
2020-09-06 12:01:55 -0300

    video footer início da estilização

--- src/components/VideoFooter.js
+++ src/components/VideoFooter.js
@@ -1,13 +1,18 @@
 import React from "react";
 
+import MusicNoteIcon from '@material-ui/icons/MusicNote';
+
 import "./VideoFooter.css";
 
 export default function VideoFooter() {
     return (
         <div className="videoFooter">
-            <div className="videoFooter_text">
+            <div className="videoFooter__text">
                 <h3>@arthursantosgc</h3>
                 <p>This is some description</p>
+                <div className="videoFooter_ticker">
+                    <MusicNoteIcon className="videoFooter_icon" />
+                </div>
             </div>
         </div>
     );
