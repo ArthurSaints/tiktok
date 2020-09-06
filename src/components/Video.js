47f4bcec7e78529b8ad53850999fe1ce307fc16f
Arthur Santos <arthursantosgc@gmail.com>
2020-09-06 11:42:56 -0300

    material icons adicionado e video footer e sidebar components criados

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,5 +1,8 @@
 import React, { useRef, useState } from "react";
 
+import VideoFooter from "./VideoFooter";
+import VideoSidebar from "./VideoSidebar";
+
 import "./Video.css";
 
 export default function Video() {
@@ -26,8 +29,8 @@ export default function Video() {
                 src="https://v16m.tiktokcdn.com/a12af41c35eea865ad8ba2c6df99ec4a/5f554179/video/tos/useast2a/tos-useast2a-pve-0068/df91e65875e8443387d62ce46707f765/?a=1233&br=1748&bt=874&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202009061407070101902090881605A174&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=ajxpa2t3ZXI1dTMzNTczM0ApPDk3M2Y4ZDtkNzM3NTs3NWdmZXNuXzZhLWpfLS1fMTZzcy5jLTQvXjUwMzItYTAuLS06Yw%3D%3D&vl=&vr=">
             </video>
 
-            {/* video footer */}
-            {/* video sidebar */}
+            <VideoSidebar></VideoSidebar>
+            <VideoFooter></VideoFooter>
         </div>
     );
 }
---
+++ src/components/VideoFooter.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./VideoFooter.css";
+
+export default function VideoFooter() {
+    return (
+        <div className="videoFooter">
+            <div className="videoFooter_text">
+                <h3>@arthursantosgc</h3>
+                <p>This is some description</p>
+            </div>
+        </div>
+    );
+}
