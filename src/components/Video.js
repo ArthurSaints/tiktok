2b96c095eee3f0c4892ea1adaad8261fc5077d21
Arthur Santos <arthursantosgc@gmail.com>
2020-09-07 09:44:00 -0300

    video footer finalizado

--- src/components/Video.js
+++ src/components/Video.js
@@ -26,7 +26,7 @@ export default function Video() {
                 className="video__player"
                 loop
                 ref={videoRef}
-                src="https://v16m.tiktokcdn.com/a12af41c35eea865ad8ba2c6df99ec4a/5f554179/video/tos/useast2a/tos-useast2a-pve-0068/df91e65875e8443387d62ce46707f765/?a=1233&br=1748&bt=874&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202009061407070101902090881605A174&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=ajxpa2t3ZXI1dTMzNTczM0ApPDk3M2Y4ZDtkNzM3NTs3NWdmZXNuXzZhLWpfLS1fMTZzcy5jLTQvXjUwMzItYTAuLS06Yw%3D%3D&vl=&vr=">
+                src="https://v16m.tiktokcdn.com/3689b15c0c35a41371fa7437a1388abd/5f567a11/video/tos/useast2a/tos-useast2a-ve-0068c001/66b4be845b5f44b793d5e52cbcc5900c/?a=1233&br=1876&bt=938&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=2020090712204501018907203433007189&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=anRyZ2psaXhydzMzPDczM0ApMztkNDw8NWQ6NzRpZzg7Z2c2NF5jMWtkaDNfLS0zMTZzczE1MV42LmMyLTM2NjJgMF86Yw%3D%3D&vl=&vr=">
             </video>
 
             <VideoSidebar></VideoSidebar>
--- src/components/VideoFooter.js
+++ src/components/VideoFooter.js
@@ -1,19 +1,36 @@
 import React from "react";
+import Ticker from "react-ticker";
 
 import MusicNoteIcon from '@material-ui/icons/MusicNote';
 
 import "./VideoFooter.css";
 
 export default function VideoFooter() {
+    const song = "Icon by Jaden Smith";
+
     return (
         <div className="videoFooter">
             <div className="videoFooter__text">
                 <h3>@arthursantosgc</h3>
                 <p>This is some description</p>
-                <div className="videoFooter_ticker">
-                    <MusicNoteIcon className="videoFooter_icon" />
+                <div className="videoFooter__ticker">
+                    <MusicNoteIcon
+                        className="videoFooter_icon" />
+
+                    <Ticker mode="smooth" speed={5}>
+                        {({ index }) => (
+                            <>
+                                <p>{song}</p>
+                            </>
+                        )}
+                    </Ticker>
                 </div>
             </div>
+            <img
+                className="videoFooter__record"
+                src="https://static.thenounproject.com/png/934821-200.png"
+                alt=""
+            />
         </div>
     );
 }
