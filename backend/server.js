c8bf0022270e64d62054cd66b579afbcd1a8f304
Arthur Santos <arthursantosgc@gmail.com>
2020-09-07 12:02:23 -0300

    get e post funcionando com o mongodb

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const app = express();
 const port = 9000;
 
 // middlewares
+app.use(express.json());
 
 // db config 
 const connection_url = "mongodb+srv://admin:stxW368cWdOwlC81@cluster0.ovkrl.mongodb.net/tiktok-clone-db?retryWrites=true&w=majority";
@@ -22,6 +23,29 @@ mongoose.connect(connection_url, {
 // api endpoints
 app.get("/", (req, res) => res.status(200).send("Hello World!"));
 app.get("/v1/posts", (req, res) => res.status(200).send(Data));
+app.get("/v2/posts", (req, res) => {
+    Videos.find((err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(data);
+        }
+    });
+});
+
+app.post("/v2/posts", (req, res) => {
+    // POST request is to ADD DATA to the database
+    // It will let us ADD a video DOCUMENTO to the videos COLLECTION
+    const dbVideos = req.body;
+
+    Videos.create(dbVideos, (err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(201).send(data);
+        }
+    });
+});
 
 // listen
 app.listen(port, () => console.log(`Listening on localhost:${port}`));
